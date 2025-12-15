ecdaf347a5e2dd2e95f165797e15c1d96d9e1efd
Jesuiskoriel <lajimijhawad@gmail.com>
2025-12-15 16:20:29 +0100

    BIG UPDATE
    Tout mon code marche première apparition des interface graphique

--- src/App.java
+++ src/App.java
@@ -4,21 +4,32 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
 public class App extends Application {
+
     @Override
     public void start(Stage primaryStage) {
 
-        Parent root;
-
         try {
-            
-            root = FXMLLoader.load(getClass().getResource("/views/main.fxml"));
-            Scene scene = new Scene(root, 800, 600); 
+            // 🔎 Vérification explicite du chemin du FXML
+            URL fxmlUrl = getClass().getResource("/views/main.fxml");
+
+            if (fxmlUrl == null) {
+                throw new RuntimeException("Impossible de trouver /views/main.fxml");
+            }
+
+            FXMLLoader loader = new FXMLLoader(fxmlUrl);
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root, 800, 600);
 
             primaryStage.setTitle("Système de Gestion de Billetterie");
             primaryStage.setScene(scene);
             primaryStage.show();
+
         } catch (Exception e) {
+            System.err.println("ERREUR AU CHARGEMENT DE L'APPLICATION");
             e.printStackTrace();
         }
     }
@@ -27,43 +38,3 @@ public class App extends Application {
         launch(args);
     }
 }
-// import javafx.application.Application;
-// import jjava java avafx.event.ActionEvent;
-// import javafx.event.EventHandler;
-// import javafx.scene.Scene;
-// import javafx.scene.control.Button;
-// import javafx.scene.layout.StackPane;
-// import javafx.stage.Stage;
- 
-// public class App extends Application {
-//     @Override
-//     public void start(Stage primaryStage) {
-//         Button btn = new Button();
-//         btn.setText("Say 'Hello World'");
-//         btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-//             @Override
-//             public void handle(ActionEvent event) {
-//                 System.out.println("Hello World!");
-//             }
-//         });
-        
-//         StackPane root = new StackPane();
-//         root.getChildren().add(btn);
-  
-//   /*
-//   Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
-//   Scene scene = new Scene(root);
-//   */
-  
-//   Scene scene = new Scene(root, 300, 250);
-  
-//   primaryStage.setTitle("Hello World!");
-//         primaryStage.setScene(scene);
-//         primaryStage.show();
-//     }
- 
-//  public static void main(String[] args) {
-//         launch(args);
-//     }
-// }
